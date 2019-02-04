ced366b5d9f3d7f864cc7d9e4183c92eb31ff846
Bart de Bever <bartdebever112@gmail.com>
2019-02-04 16:06:19 +0100

    First bean setup

---
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/beans/UserBean.java
@@ -0,0 +1,39 @@
+package com.bartdebever.twetter.beans;
+
+import com.bartdebever.twetter.beans.interfaces.IUserBean;
+import com.bartdebever.twetter.models.NewUser;
+import com.bartdebever.twetter.models.User;
+import com.bartdebever.twetter.services.UserService;
+
+import java.util.List;
+
+public class UserBean implements IUserBean {
+
+    //@Inject
+    public UserService userService;
+
+    @Override
+    public User getUser(int id) {
+        return null;
+    }
+
+    @Override
+    public List<User> GetAllUsers() {
+        return null;
+    }
+
+    @Override
+    public void addUser(NewUser user) {
+
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void removeUser(User user) {
+
+    }
+}
---
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/beans/interfaces/IUserBean.java
@@ -0,0 +1,18 @@
+package com.bartdebever.twetter.beans.interfaces;
+
+import com.bartdebever.twetter.models.NewUser;
+import com.bartdebever.twetter.models.User;
+
+import java.util.List;
+
+public interface IUserBean {
+    User getUser(int id);
+
+    List<User> GetAllUsers();
+
+    void addUser(NewUser user);
+
+    void updateUser(User user);
+
+    void removeUser(User user);
+}
