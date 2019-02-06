94a2ff6278029bc6a694c366c4d8a21042543aca
Bart de Bever <bartdebever112@gmail.com>
2019-02-06 11:55:47 +0100

    Documented CRUDService

--- Backend/twetter/src/main/java/com/bartdebever/twetter/services/CrudService.java
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/services/CrudService.java
@@ -4,18 +4,34 @@ import com.bartdebever.twetter.models.interfaces.IEntity;
 
 import javax.persistence.EntityManager;
 
+/**
+ * An abstract class used to perform some CRUD actions (Create, Read, Update, Delete)
+ * @param <T> the type that the entity is wanting to perform these actions on.
+ */
 public abstract class CrudService<T extends IEntity> {
 
     private EntityManager entityManager;
 
+    /**
+     * Updates an entity in the underlying database.
+     * @param entity the entity wanting to be updated.
+     */
     public void update(T entity) {
         entityManager.merge(entity);
     }
 
+    /**
+     * Deletes an entity from the underlying database.
+     * @param entity the entity wanting to be removed.
+     */
     public void delete(T entity) {
         entityManager.remove(entityManager.merge(entity));
     }
 
+    /**
+     * Inserts an entity into the underlying database.
+     * @param entity the entity wanting to be inserted.
+     */
     public void insert(T entity) {
         entityManager.persist(entity);
     }
