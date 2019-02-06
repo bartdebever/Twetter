b29e075deeb929f7557801e8a8788f9e980a9a30
Bart de Bever <bartdebever112@gmail.com>
2019-02-06 11:32:07 +0100

    Added TwitBean interface

---
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/beans/interfaces/ITwitBean.java
@@ -0,0 +1,48 @@
+package com.bartdebever.twetter.beans.interfaces;
+
+import com.bartdebever.twetter.models.Twit;
+import com.bartdebever.twetter.models.User;
+
+import java.util.List;
+
+public interface ITwitBean {
+
+    /**
+     * Posts a Twit to the application.
+     * @param twit the twit wanting to be posted.
+     */
+    void postTwit(Twit twit);
+
+    /**
+     * Removes a Twit from the application.
+     * @param twit the twit wanting to be removed.
+     */
+    void removeTwit(Twit twit);
+
+    /**
+     * Gets a Twit from the application based on it's id.
+     * @param id the id of the Twit wanting to be looked for.
+     * @return the Twit object or null.
+     */
+    Twit getTwit(int id);
+
+    /**
+     * Gets all Twits in the application.
+     * @return a collection of Twits.
+     */
+    List<Twit> getAllTwits();
+
+    /**
+     * Gets the Twits that are featured on the user's timeline.
+     * @param user the user wanting the Twits for.
+     * @return a collection of Twit objects.
+     */
+    List<Twit> getTimelineTwits(User user);
+
+    /**
+     * Gets the Twits made by a single user.
+     * @param user the user wanting to be looked for.
+     * @return a collection of Twit objects.
+     */
+    List<Twit> getTwitsByUser(User user);
+}
