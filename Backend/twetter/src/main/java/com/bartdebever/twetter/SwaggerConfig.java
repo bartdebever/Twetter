c2c5d171b2ef3786e7973ddcc223e1dc19b1a5c7
Bart de Bever <bartdebever112@gmail.com>
2019-02-07 15:08:44 +0100

    Added API info to swagger

--- Backend/twetter/src/main/java/com/bartdebever/twetter/SwaggerConfig.java
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -20,8 +24,19 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.bartdebever.twetter.controllers"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Twetter API.",
+                "The best Twitter ripoff.",
+                "0.1",
+                "There is none.",
+                new Contact("Bart de Bever", "www.twetter.com", "bart@twetter.com"),
+                "Whatever license you want.", "There is none!", Collections.emptyList());
     }
 }
