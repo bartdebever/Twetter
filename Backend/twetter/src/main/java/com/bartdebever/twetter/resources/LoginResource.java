79e5b6dd5e3f1ef87381ecc944268ff6b663ff0f
Bart de Bever <bartdebever112@gmail.com>
2019-02-18 12:02:48 +0100

    Resources javadoc

--- Backend/twetter/src/main/java/com/bartdebever/twetter/resources/LoginResource.java
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/resources/LoginResource.java
@@ -4,18 +4,34 @@ public class LoginResource {
     private String username;
     private String password;
 
+    /**
+     * Gets the password of the login.
+     * @return the set password.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Sets the password for the login.
+     * @param password the password intending to be set.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Gets the username used for the login.
+     * @return the username.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets the username for the login.
+     * @param username the username wanting to be set.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
