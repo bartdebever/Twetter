1df3cce78c4d77daf8229cdd300dd9d8e05bbac3
Bart de Bever <bartdebever112@gmail.com>
2019-02-18 12:04:51 +0100

    Services javadoc

--- Backend/twetter/src/main/java/com/bartdebever/twetter/services/interfaces/IUserService.java
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/services/interfaces/IUserService.java
@@ -3,7 +3,17 @@ package com.bartdebever.twetter.services.interfaces;
 import com.bartdebever.twetter.models.User;
 
 public interface IUserService extends ICrudService<User> {
+    /**
+     * Adds a user to the other user's following list.
+     * @param followerId the user who will be following the other.
+     * @param followingId the user being followed.
+     */
     void addFollow(int followerId, int followingId);
 
+    /**
+     * Searches the application for a user with the given name.
+     * @param username the username wanting to be searched for.
+     * @return an user object or null.
+     */
     User searchByName(String username);
 }
