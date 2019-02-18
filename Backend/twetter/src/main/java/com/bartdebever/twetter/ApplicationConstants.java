912c3c93cc09bd402d462251a7500a42ea04f061
Bart de Bever <bartdebever112@gmail.com>
2019-02-18 10:29:33 +0100

    Unit tests for JwtTokenGen

--- Backend/twetter/src/main/java/com/bartdebever/twetter/ApplicationConstants.java
+++ Backend/twetter/src/main/java/com/bartdebever/twetter/ApplicationConstants.java
@@ -3,7 +3,7 @@ package com.bartdebever.twetter;
 public class ApplicationConstants {
     public final static String APPLICATION_NAME = "Twetter";
 
-    public final static String JWT_SECRET = "TwetterSecretKey";
+    public final static String JWT_SECRET = "TwetterSecretKeyWhichIsVerySecretAndGoodPleaseLetThisBeGoodEnough";
 
     public final static String CONTROLLER_PACKAGE = "com.bartdebever.twetter.controllers";
 
