b64d6b82e97bc6f84fbe811ac1c8ec8d219230f7
Bart de Bever <bartdebever112@gmail.com>
2019-04-24 23:03:31 +0200

    Created service tests

--- Backend/src/test/kotlin/services/UserServiceTest.kt
+++ Backend/src/test/kotlin/services/UserServiceTest.kt
@@ -1,27 +1,117 @@
 package services
 
-import org.jboss.weld.junit5.WeldInitiator
-import org.jboss.weld.junit5.WeldJunit5Extension
-import org.jboss.weld.junit5.WeldSetup
+import models.User
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import services.interfaces.IUserService
 import services.local.LocalUserService
-import javax.enterprise.context.RequestScoped
-import javax.inject.Inject
 
 
-@ExtendWith(WeldJunit5Extension::class)
 open class UserServiceTest {
 
-    @WeldSetup
-    var weld = WeldInitiator.from(LocalUserService::class.java).activate(RequestScoped::class.java).build()
-
     private var userService: IUserService? = null
+    private val userName = "TestUser"
+    private val password = "Welcome1!"
+    private val invalidId = -100
+    private val notFoundId = 200
+
+    @BeforeEach
+    open fun initialize() {
+        userService = LocalUserService()
+    }
+    //region getUserTest
+    @Test
+    fun getUserTest() {
+        val user = insertNewUser()
+        val receivedUser = userService!!.getById(user.id)
+
+        assertSame(user, receivedUser)
+    }
 
     @Test
-    open fun usernamePasswordLoginTest() {
-        userService = weld.select(LocalUserService::class.java).get()
-        assert(userService != null)
+    fun getUserBadFlowNotFoundTest() {
+        val receivedUser = userService!!.getById(notFoundId)
+
+        assertNull(receivedUser)
+    }
+
+    @Test
+    fun getUserBadFlowInvalidIdTest() {
+        assertThrows(IllegalArgumentException::class.java) { userService!!.getById(invalidId) }
+    }
+    //endregion
+    //region insertUserTest
+    @Test
+    fun insertUserTest() {
+        val user = insertNewUser()
+        val receivedUser = userService!!.getById(user.id)!!
+
+        assertSame(user, receivedUser)
+    }
+    //endregion
+
+    //region removeUserTest
+    @Test
+    fun removeUserTest() {
+        val user = insertNewUser()
+        userService!!.delete(user)
+
+        val receivedUser : User? = userService!!.getById(user.id)
+
+        assertNull(receivedUser)
+    }
+    //endregion
+    //region userLoginTest
+    @Test
+    open fun userLoginTest() {
+        val user = insertNewUser()
+        val loggedInUser: User? = userService!!.validateUser(userName, password)
+
+        assertNotNull(loggedInUser)
+        assertEquals(user.id, loggedInUser!!.id)
+        assertSame(user, loggedInUser)
+    }
+
+    @Test
+    fun userLoginBothBadFlowTest() {
+        val receivedUser = userService!!.validateUser("Does not", "Exist")
+
+        assertNull(receivedUser)
+    }
+
+    @Test
+    fun userLoginUsernameBadFlowTest() {
+        insertNewUser()
+        val receivedUser = userService!!.validateUser("Is not correct", password)
+
+        assertNull(receivedUser)
+    }
+
+    @Test
+    fun userLoginPasswordBadFlow() {
+        insertNewUser()
+        val receivedUser = userService!!.validateUser(userName, "Is not correct")
+
+        assertNull(receivedUser)
+    }
+
+    //endregion
+
+
+    private fun createUser(): User {
+        val user = User()
+        user.id = 1
+        user.userName = userName
+        user.password = password
+
+        return user;
+    }
+
+    private fun insertNewUser(): User {
+        val user = createUser()
+        userService!!.insert(user)
+
+        return user;
     }
 }
